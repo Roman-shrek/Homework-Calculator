06a683be41b28bd220515270317a4401c8806f64
roman <rom.pyatakov@yandex.ru>
2024-11-08 21:51:23 +0300

    Init

---
+++ src/test/java/pro/sky/skycalculator/SkyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
